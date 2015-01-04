23a61a8f55e218c71ab00be512a73a17e8d42a59
Mohan <mohanmca@gmail.com>
2015-01-05 06:11:03 +0800

    Commit

---
+++ src/main/scala/ground/learning/scala/pf/example/EmptyPf.scala
@@ -0,0 +1,11 @@
+package ground.learning.scala.pf.example
+
+object EmptyPf {
+  def emptyPfOne(value: AnyRef): Unit = value match {
+    case _ if false => ()
+  }
+
+  val emptyPf: PartialFunction[AnyRef, Unit] = { case _ if false => () }
+
+  val emptyPfWinner: PartialFunction[AnyRef, Unit] = Map()
+}
