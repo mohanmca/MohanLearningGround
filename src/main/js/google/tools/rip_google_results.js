b163df31fba7af6ec85d2362e4f50db37eb1a99d
mohanmca <mohanmca@gmail.com>
2017-07-31 22:59:38 +0800

    download multiple files

--- src/main/js/google/tools/rip_google_results.js
+++ src/main/js/google/tools/rip_google_results.js
@@ -12,20 +12,23 @@ function downloadURI(uri, name) {
   document.body.removeChild(link);
   delete link;
 }
-var container = document.evaluate('//*[@id="rso"]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-var results = Array.from(container.singleNodeValue.children)
-var resultLinks = results.map(childNode =>  document.evaluate('div/div/h3/a', childNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null))
-var result = resultLinks.map( node  => 
-	{
-		var o = new Object()
-		o['href']	 =  node.singleNodeValue.href
-		o['text'] =   node.singleNodeValue.outerText
-		return o;
-	} 
-)
-console.table(result)
-var links = result.map(r => r.href)
-links.map(l => downloadURI(l,l))
+function parseLinks() {
+    var container = document.evaluate('//*[@id="rso"]/div/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+    var results = Array.from(container.singleNodeValue.children)
+    var resultLinks = results.map(childNode =>  document.evaluate('div/div/h3/a', childNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null))
+    var result = resultLinks.map( node  => 
+        {
+            var o = new Object()
+            o['href']    =  node.singleNodeValue.href
+            o['text'] =   node.singleNodeValue.outerText
+            return o;
+        } 
+    )
+    console.table(result)
+    var links = result.map(r => r.href)
+    return links;
+}
+parseLinks().map(l => downloadURI(l,l))
 
 
 /**
