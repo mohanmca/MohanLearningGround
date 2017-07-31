52cc727c4b3f7b39aa02b2eb67fc3bd261d15cc7
mohanmca <mohanmca@gmail.com>
2017-08-01 07:03:35 +0800

    refactor folders

--- .worksheet/src/ground.worksheet.lazy_stream_ws.scala
+++
@@ -1,9 +0,0 @@
-package ground.worksheet
-
-import ground.learning.lasy.streams._
-
-object lazy_stream_ws {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(134); 
-  println("Welcome to the Scala worksheet");$skip(45); 
-  Streams.natNumbers.take(3) foreach println;$skip(39); 
-  Streams.fibs take 10 foreach println}
-}
