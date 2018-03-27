ddb02e95c62813d5cc63cced19be4f2dcecca772
Mohan <mohanmca@gmail.com>
2018-03-27 22:37:39 +0800

    OrderType and OrderProcessor

--- src/main/java/oms/OrderProcessor.java
+++ src/main/java/oms/OrderProcessor.java
@@ -2,35 +2,76 @@ package oms;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.logging.Logger;
 
+/**
+ * Best Buy Order - Highest bid price
+ * ---- 
+ * Best Sell Order - Lowest asking price
+ * 
+ * @author mohan
+ */
 public class OrderProcessor {
-	private Set<Order> bestSell = new TreeSet<Order>(new BestSellOrder());
-	private Map<Integer, Order> orders = new ConcurrentHashMap<Integer, Order>();
+
+	private static Logger logger = Logger.getLogger("com.wombat.nose");
+
+	private ConcurrentSkipListSet<Order> bestSell = new ConcurrentSkipListSet<Order>(new BestOrder().reversed());
+	private ConcurrentSkipListSet<Order> bestBuy = new ConcurrentSkipListSet<Order>(new BestOrder());
+	private Map<Long, Order> orders = new ConcurrentHashMap<Long, Order>();
+	private static OrderProcessor _instance = new OrderProcessor();
+
+	private OrderProcessor() {
+	}
+
+	public static OrderProcessor getInstance() {
+		return _instance;
+	}
+
+	public void clear() {
+		bestSell.clear();
+		bestBuy.clear();
+		orders.clear();
+	}
 
 	public void processOrder(Order order) {
-		Order oldOrder = orders.putIfAbsent(order.getOrderId(), order);
-		if (oldOrder.equals(order)) {
-			// just inserted - new order..
+		Order oldOrder = orders.put(order.getOrderId(), order);
+		if (oldOrder == null || !oldOrder.equals(order)) {
+			logger.finer("New order inserted! - oldOrder " + oldOrder + ", new order" + order.toString());
 		} else {
-			// old one updated - old order..
-			trigger(order);
+			logger.finer("Existing order updated!" + order);
 		}
+		trigger(order);
 	}
 
 	private void trigger(Order order) {
-		bestSell.add(order);
-		System.out.println(bestSell.iterator().next());
-	}
-
-	static class BestSellOrder implements java.util.Comparator<Order> {
-		@Override
-		public int compare(Order o1, Order o2) {
-			return o1.getPrice() - o1.getPrice();
+		if (order.getType() == OrderType.BUY) {
+			bestBuy.add(order);
+		} else {
+			bestSell.add(order);
 		}
 	}
-	
+
+	static class BestOrder implements java.util.Comparator<Order> {
+		@Override
+		public int compare(Order o1, Order o2) {
+			return o1.getPrice() - o2.getPrice();
+		}
+	}
+
+	public int traderBlotterSize() {
+		return bestSell.size() + bestBuy.size();
+	}
+
+	public Order bestOrder(OrderType type) {
+		if (type == OrderType.BUY) {
+			return bestBuy.last();
+		}
+		return bestSell.last();
+	}
+
 	public static void main(String args[]) {
-		
+
 	}
 
 }
---
+++ src/test/java/oms/OrderProcessorTest.java
@@ -0,0 +1,53 @@
+package oms;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Optional;
+
+public class OrderProcessorTest {
+
+	@Before
+	public void clear() {
+		OrderProcessor.getInstance().clear();
+	}
+
+	@Test
+	public void singleOrder() {
+		Order order = Order.create(Optional.<Order>empty());
+		OrderProcessor.getInstance().processOrder(order);
+		Assert.assertTrue(OrderProcessor.getInstance().bestOrder(OrderType.SELL).equals(order));
+	}
+
+	@Test
+	public void bestOfTwoSellOrders() {
+		Order modelOrder = Order.create(Optional.empty());
+		Order orderSecond = Order.create(Optional.empty());
+		modelOrder.setPrice(10);
+		orderSecond.setPrice(modelOrder.getPrice() - 1);
+
+		OrderProcessor.getInstance().processOrder(modelOrder);
+		OrderProcessor.getInstance().processOrder(orderSecond);
+
+		Assert.assertEquals(OrderProcessor.getInstance().traderBlotterSize(), 2);
+		Assert.assertEquals(OrderProcessor.getInstance().bestOrder(OrderType.SELL), orderSecond);
+		Assert.assertEquals(OrderProcessor.getInstance().bestOrder(OrderType.SELL).getPrice(), orderSecond.getPrice());
+	}
+
+	@Test
+	public void bestOfTwoBuyOrders() {
+		Order modelOrder = Order.create(Optional.empty());
+		modelOrder.setType(OrderType.BUY);
+		modelOrder.setPrice(10);
+
+		Order orderSecond = Order.create(Optional.of(modelOrder));
+		orderSecond.setPrice(modelOrder.getPrice() + 10);
+
+		OrderProcessor.getInstance().processOrder(modelOrder);
+		OrderProcessor.getInstance().processOrder(orderSecond);
+
+		Assert.assertEquals(OrderProcessor.getInstance().traderBlotterSize(), 2);
+		Assert.assertEquals(OrderProcessor.getInstance().bestOrder(OrderType.BUY), orderSecond);
+		Assert.assertEquals(OrderProcessor.getInstance().bestOrder(OrderType.BUY).getPrice(), orderSecond.getPrice());
+	}
+}
