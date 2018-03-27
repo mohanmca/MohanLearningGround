b02f51bb4115701e0b57f45a4b0ce1f43817e4b7
Mohan <mohanmca@gmail.com>
2018-03-27 22:37:39 +0800

    OrderType and OrderProcessor

--- src/main/java/oms/Order.java
+++ src/main/java/oms/Order.java
@@ -1,17 +1,20 @@
 package oms;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class Order {
-	//dummy for 
+	// dummy for
 	static Random r = new java.util.Random(System.currentTimeMillis());
 	private long orderId;
 	private int version;
 	private int price;
-	
-	private Order() {	
+	private String userId;
+	private OrderType type;
+
+	private Order() {
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
@@ -19,9 +22,6 @@ public class Order {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
-	private String userId;
-	private OrderType type;
 
 	public long getOrderId() {
 		return orderId;
@@ -51,16 +51,47 @@ public class Order {
 		return type;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (orderId ^ (orderId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (orderId != other.orderId)
+			return false;
+		return true;
+	}
+
 	public void setType(OrderType type) {
 		this.type = type;
 	}
-	
-	public static Order create(order Optional) {
-		Order o  = new Order();
-		o.setUserId("userId");
-		o.setOrderId(System.currentTimeMillis());
-		o.setPrice(r.nextInt(5));
-		o.setVersion(version);
+
+	public static Order create(Optional<Order> modelOrder) {
+		Order o = new Order();
+		if (modelOrder.isPresent()) {
+			o.setUserId(modelOrder.get().getUserId());
+			o.setOrderId(modelOrder.get().getOrderId());
+			o.setPrice(modelOrder.get().getPrice());
+			o.setVersion(modelOrder.get().getVersion() + 1);
+			o.setType(modelOrder.get().getType());
+		} else {
+			o.setUserId("userId");
+			o.setOrderId(System.currentTimeMillis());
+			o.setPrice(r.nextInt(5));
+			o.setType(OrderType.SELL);
+			o.setVersion(1);
+		}
 		return o;
 	}
 
